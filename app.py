a8c87f61950021857b62d272fbcb72ff204d17d6
crypticCaroline <rebecca.kelsall@sky.com>
2021-02-23 23:17:35 +0000

    Added basic building of profile/update to registration

--- app.py
+++ app.py
@@ -38,55 +38,85 @@ def register():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password")),
+            "dog_name": request.form.get("dog_name"),
+            "dog_description": request.form.get("dog_description")
         }
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-        return redirect(url_for("profile", username=session["user"]))
+        return redirect(url_for("buildprofile", username=session["user"]))
     return render_template("register.html")
 
 
-@app.route("/profile/<username>", methods=["GET", "POST"])
+@app.route("/profile/buildprofile/<username>", methods=["GET", "POST"])
+def buildprofile(username):
+
+    this_user = mongo.db.users.find_one({"username": session["user"]})
+    print(this_user)
+
+    if request.method == "POST":
+        mongo.db.users.update_one(
+            {"username": session["user"]},
+            {"$set": {
+                "dog_name": request.form.get("dog_name"),
+                "dog_description": request.form.get("dog_description")
+            }}
+        )
+
+        flash("Task Successfully Updated")
+        return redirect(url_for("profile",  username=session["user"]))
+
+    return render_template("buildprofile.html", username=session["user"])
+
+
+@ app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
-    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+
 
     if session["user"]:
-        return render_template("profile.html", username=username)
+        return render_template("profile.html", username = username)
 
     return redirect(url_for('homepage'))
 
 
-@app.route("/login", methods=["GET", "POST"])
+@ app.route("/login", methods = ["GET", "POST"])
 def login():
     if request.method == "POST":
-        existing_user = mongo.db.users.find_one(
+        existing_user=mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            #ensure hash matches
+            # ensure hash matches
             if check_password_hash(
                     existing_user["password"], request.form.get("password")):
-                session["user"] = request.form.get("username").lower()
+                session["user"]=request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
                 return redirect(url_for(
                     "profile", username=session["user"]))
             else:
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
-
         else:
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
     return render_template("login.html")
 
 
+@ app.route("/logout")
+def logout():
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
-            debug=True)
+    app.run(host = os.environ.get("IP"),
+            port = int(os.environ.get("PORT")),
+            debug = True)
 
 
 # @app.errorhandler(werkzeug.exceptions.BadRequest)
