a9b7c7eb4e26b6e844b12bc21b86700e830d967c
crypticCaroline <rebecca.kelsall@sky.com>
2021-02-24 14:39:21 +0000

    Updated profile to read from database

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
-$( document ).ready(function(){
+$(document).ready(function () {
     $(".button-collapse").sideNav();
+    $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false, // Close upon selecting a date,
+        container: undefined, // ex. 'body' will append picker to body
+    });
+
+
 })
+
