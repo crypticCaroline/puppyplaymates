77fa9142b2c59bf29838000a9502752344736e2a
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-10 15:58:05 +0000

    Added JS functionality for the overlays when users like/dislike

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('#textarea1').val('New Text');
     $('#textarea1').trigger('autoresize');
     $('select').material_select();
+    $('.modal').modal();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -13,6 +14,22 @@ $(document).ready(function () {
         container: undefined, // ex. 'body' will append picker to body
     });
 
-
 })
 
+
+like = document.getElementById('overlay_happy')
+unlike = document.getElementById('overlay_angry')
+
+function overlay_happy(){
+  let form = document.getElementById('form_like')  
+  console.log(form)
+  like.style.display = "block";
+  setTimeout(function(){ form.submit('POST'); }, 3000)
+};
+
+function overlay_angry(){
+  let form = document.getElementById('form_dislike')  
+  console.log(form)  
+  unlike.style.display = "block";
+  setTimeout(function(){ form.submit('POST'); }, 3000)
+};
