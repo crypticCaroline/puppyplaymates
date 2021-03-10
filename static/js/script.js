c6523ee622e2dc81c29edeae3babedf66a40e2b3
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-10 23:44:04 +0000

    Initalised chips

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $('#textarea1').val('New Text');
     $('#textarea1').trigger('autoresize');
     $('select').material_select();
+     $('.chips').material_chip();
     $('.modal').modal({
         inDuration: 500
     });
