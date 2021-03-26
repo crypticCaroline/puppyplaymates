5a5dad73b19d64046ad1972e15418e040a6c95a6
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-26 19:32:14 +0000

    Turning debugger off for user testing

--- app.py
+++ app.py
@@ -735,4 +735,4 @@ def internal_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
