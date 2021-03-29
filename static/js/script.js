ca54b86933a516cbe45f4ceb621bef5cc5b703fc
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-29 10:55:54 +0000

    Textarea validator issues, added a min length, need to add a check for empty spaces

--- static/js/script.js
+++ static/js/script.js
@@ -24,17 +24,17 @@ $(document).ready(function () {
 like = document.getElementById('overlay_happy')
 unlike = document.getElementById('overlay_angry')
 
-function overlay_happy(){
-  let form = document.getElementById('form_like')  
-  console.log(form)
-  like.style.display = "block";
-  setTimeout(function(){ form.submit( ); }, 3000)
+function overlay_happy() {
+    let form = document.getElementById('form_like')
+    console.log(form)
+    like.style.display = "block";
+    setTimeout(function () { form.submit(); }, 3000)
 };
 
-function overlay_angry(){
-  let form = document.getElementById('form_dislike')  
-  console.log(form)  
-  unlike.style.display = "block";
-  setTimeout(function(){ form.submit(); }, 3000)
+function overlay_angry() {
+    let form = document.getElementById('form_dislike')
+    console.log(form)
+    unlike.style.display = "block";
+    setTimeout(function () { form.submit(); }, 3000)
 };
 
