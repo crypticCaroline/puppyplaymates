a004c2670b53db10b0f3ffc630eef510135bb05c
crypticCaroline <rebecca.kelsall@sky.com>
2021-03-29 13:16:07 +0000

    BUG users could enter any date FIX Added defensive programming so can only use set dates

--- static/js/script.js
+++ static/js/script.js
@@ -20,20 +20,41 @@ $(document).ready(function () {
 
 })
 
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1;  
+let yyyy = today.getFullYear();
+// checks if the date is less than 10 so adds a 0 infront 
+ if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+
+today = yyyy+'-'+mm+'-'+dd;
+oldestYear = yyyy-21;
+oldestDog = oldestYear+'-'+mm+'-'+dd;
+eventYear = yyyy+1;
+eventDate = eventYear+'-'+mm+'-'+dd;
+
+document.getElementById("dog_dob").setAttribute("max", today);
+document.getElementById("dog_dob").setAttribute("min", oldestDog);
+document.getElementById("date").setAttribute("min", today);
+document.getElementById("date").setAttribute("max", eventYear);
+
 
 like = document.getElementById('overlay_happy')
 unlike = document.getElementById('overlay_angry')
 
 function overlay_happy() {
     let form = document.getElementById('form_like')
-    console.log(form)
     like.style.display = "block";
     setTimeout(function () { form.submit(); }, 3000)
 };
 
 function overlay_angry() {
     let form = document.getElementById('form_dislike')
-    console.log(form)
     unlike.style.display = "block";
     setTimeout(function () { form.submit(); }, 3000)
 };
