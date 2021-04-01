370c8f2f44f96f9cc927654778752820f30b12e2
crypticCaroline <rebecca.kelsall@sky.com>
2021-04-01 16:23:27 +0000

    Turning debugger off

--- app.py
+++ app.py
@@ -650,4 +650,4 @@ def internal_error(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
